1cc4ffe034d84112fff2188bed497b0fdf171260
Tbread <81889764+Tbread@users.noreply.github.com>
2021-12-31 21:54:59 +0900

    Initial commit

---
+++ src/test/java/com/payhere/pageonce/PageonceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payhere.pageonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageonceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
